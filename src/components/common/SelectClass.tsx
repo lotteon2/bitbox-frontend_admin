61f19e6ddc36acfa2f59aa1943eb4832c2ec5b7a
Hyevvy <hyeg0@naver.com>
2023-09-25 15:25:42 +0900

    💛 [FEAT] SelectClass 컴포넌트 구현

---
+++ src/components/common/SelectClass.tsx
@@ -0,0 +1,19 @@
+import { Select } from 'antd';
+
+interface SelectClassIntferface {
+	handleChange: (value: string) => void;
+	defaultValue?: string;
+	options: { value: string; label: string }[];
+}
+const SelectClass: React.FC<SelectClassIntferface> = ({ handleChange, defaultValue, options }) => {
+	return (
+		<Select
+			defaultValue={defaultValue || options[0].value}
+			style={{ width: 200 }}
+			onChange={handleChange}
+			options={options}
+		/>
+	);
+};
+
+export default SelectClass;
--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -1,20 +1,22 @@
-import React from "react";
+import React from 'react';
 import { Select } from 'antd';
+import SelectClass from '../components/common/SelectClass';
 
 export default function DashBoard() {
-    const handleChange = (value: string) => {
-        console.log(`selected ${value}`);
-      };
-    return <div className="mx-3">
-        <Select
-          defaultValue="lucy"
-          style={{ width: 120 }}
-          onChange={handleChange}
-          options={[
-            { value: 'jack', label: 'Jack' },
-            { value: 'lucy', label: 'Lucy' },
-            { value: 'Yiminghe', label: 'yiminghe' }
-          ]}
-        />
-        대시보드</div>;
+	const handleChange = (value: string) => {
+		console.log(`selected ${value}`);
+	};
+
+	// TODO: 초기 진입시 본인의 반 불러와서 options에 넣어주기
+	const options = [
+		{ value: 'jx411', label: '롯데이커머스2기' },
+		{ value: 'lucy', label: 'Lucy' },
+		{ value: 'Yiminghe', label: 'yiminghe' },
+	];
+	return (
+		<div className="mx-3">
+			<SelectClass handleChange={handleChange} options={options} />
+			대시보드
+		</div>
+	);
 }
