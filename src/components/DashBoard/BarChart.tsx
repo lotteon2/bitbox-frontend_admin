baa17e704fa1c2e596819ae815de147061adbd75
Hyevvy <hyeg0@naver.com>
2023-09-26 11:29:15 +0900

    💛 [FEAT] BarChart component 구현

---
+++ src/components/DashBoard/BarChart.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BarChart as RechartBarchart, Bar, XAxis, YAxis } from 'recharts';
+
+interface BarChartInterface {
+	chartName: string;
+	data: { name: string; num: number }[];
+}
+
+const BarChart: React.FC<BarChartInterface> = ({ chartName, data }) => {
+	return (
+		<div className="shadow-lg grayscale3 flex flex-col items-center m-5 rounded-xl">
+			<div className="font-bold text-xl">{chartName}</div>
+			<RechartBarchart width={700} height={300} data={data}>
+				<Bar dataKey="num" className="fill-primary3" style={{ padding: '10px' }} />
+				<XAxis dataKey="name" className="font-regular" />
+				<YAxis className="font-regular" />
+			</RechartBarchart>
+		</div>
+	);
+};
+
+export default BarChart;
