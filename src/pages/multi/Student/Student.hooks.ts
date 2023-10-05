1419f64b2ad1151feb23f42091d681ed4e8f7deb
Hyevvy <hyeg0@naver.com>
2023-10-05 20:52:38 +0900

    💛 [FEAT] 교육생 추가 모달 기능 구현

--- src/pages/multi/Student/Student.hooks.ts
+++ src/pages/multi/Student/Student.hooks.ts
@@ -1,22 +1,50 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Toast } from '../../../components/common/Toast';
 
 export const useStudentModal = () => {
 	const [isModalOpen, setIsModalOpen] = useState(false);
 	const [loading, setLoading] = useState(false);
+	const [email, setEmail] = useState<string>('');
+	const [invitedEmails, setInvitedEmails] = useState<string[]>([]);
+
+	/* TODO : 서버통신 */
+	const handleClickAddBtn = (e: React.MouseEvent<Element, MouseEvent>) => {
+		console.log(email);
+		if (!email) {
+			Toast(false, '이메일을 입력해주세요.');
+		} else {
+			/* TODO : 서버통신 */
+			setInvitedEmails((prev) => [...prev, email]);
+			setEmail('');
+			Toast(true, '교육생이 초대되었습니다.');
+		}
+	};
 
 	const showModal = () => {
 		setIsModalOpen(true);
 	};
 
+	/* TODO : 서버통신 */
 	const handleOk = () => {
 		setIsModalOpen(false);
+		setInvitedEmails([]);
 	};
 
 	const handleCancel = () => {
 		setIsModalOpen(false);
+		setInvitedEmails([]);
 	};
 
+	useEffect(() => {
+		setEmail('');
+	}, [invitedEmails]);
+
 	return {
+		email,
+		setEmail,
+		handleClickAddBtn,
+		invitedEmails,
+		setInvitedEmails,
 		isModalOpen,
 		setIsModalOpen,
 		loading,
