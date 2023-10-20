f58bea74e233e455059f421d527e54e2908d1d75
Hyevvy <hyeg0@naver.com>
2023-10-20 19:09:48 +0900

    💛 [REFACTOR] login check 여부 리팩토링

--- src/App.hooks.tsx
+++ src/App.hooks.tsx
@@ -1,8 +1,6 @@
-import { useRef, useEffect } from 'react';
-import { NavigateFunction, useNavigate } from 'react-router-dom';
+import { useRef } from 'react';
 import { createQueryClient } from './libs/core/react-query/core';
 import { useUserStore } from './stores/user/user.store';
-import useMovePage from './hooks/useMovePage';
 
 export const useAppMount = () => {
 	const queryClientRef = useRef(createQueryClient());
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 import Header from '../components/common/Header';
 import TopHeader from '../components/common/TopHeader';
+import { useUserStore } from '../stores/user/user.store';
 
 export default function MainLayout() {
+	const navigate = useNavigate();
+	const [isLogin, isFirstLogin] = useUserStore((state) => [state.isLogin, state.isFirstLogin]);
+
+	useEffect(() => {
+		if (!isLogin) {
+			if (!isFirstLogin) {
+				navigate('/first');
+			} else navigate('/login');
+		}
+	}, [isLogin, isFirstLogin, navigate]);
+
 	return (
 		<div className="w-full h-screen flex flex-row scrollbar-hide">
 			<div className="w-[300px] sm:w-[200px] lg:w-[300px] h-full font-regular lg:text-3xl sm:text-sm">
