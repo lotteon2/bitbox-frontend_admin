c1cfc1f04955b20c67dd1ebb95996f7b3fd77cac
Hyevvy <hyeg0@naver.com>
2023-10-27 17:25:55 +0900

    [TEST] 중간 점검

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
 	app.use(
-		createProxyMiddleware('/authentication-service', {
+		createProxyMiddleware('/authentication-service/auth/admin', {
 			// 백엔드 주소
 			target: 'https://bitbox.kro.kr',
 			changeOrigin: true,
