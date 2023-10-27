952e736abbadae67fab00c4addbe274e47317865
Hyevvy <hyeg0@naver.com>
2023-10-27 09:11:28 +0900

    💛 [FEAT] zustand persist 추가

--- src/stores/attendanceSearch/attendanceSearch.store.ts
+++ src/stores/attendanceSearch/attendanceSearch.store.ts
@@ -1,7 +1,10 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 import dayjs from 'dayjs';
 import { AttendanceSearchState, AttendanceSearchDispatcher } from './attendanceSearch.types';
+
+const StorageKey = 'storage-key';
 
 const getToday = () => {
 	const now = dayjs();
@@ -14,13 +17,20 @@ const initialState: AttendanceSearchState = {
 };
 
 export const useAttendanceSearchStore = create(
-	immer<AttendanceSearchDispatcher>((set) => ({
-		...initialState,
-		dispatchSelectedDateString: (value: string) => {
-			set({ selectedDateString: value });
+	persist<AttendanceSearchDispatcher>(
+		(set) => ({
+			...initialState,
+			dispatchSelectedDateString: (value: string) => {
+				set({ selectedDateString: value });
+			},
+			dispatchSearchName: (value: string) => {
+				set({ searchName: value });
+			},
+		}),
+		{
+			name: StorageKey,
+			storage: createJSONStorage(() => sessionStorage),
+			// partialize: (state) => ({ selectedDateString: state.selectedDateString, searchName: state.searchName }),
 		},
-		dispatchSearchName: (value: string) => {
-			set({ searchName: value });
-		},
-	})),
+	),
 );
