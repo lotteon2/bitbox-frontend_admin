8991c86035bdbb02f8e47174570e2bb2e4e3e50d
Hyevvy <hyeg0@naver.com>
2023-10-31 16:35:34 +0900

    💛 [FIX] admin초대시 classId -1인 오류 수정

--- src/mutations/useCreateAdminMutation.ts
+++ src/mutations/useCreateAdminMutation.ts
@@ -3,15 +3,20 @@ import { adminApi } from '../apis/admin/adminAPIService';
 import { CreateAdminParams } from '../apis/admin/adminAPIService.types';
 import { useMutation } from '../libs/core/react-query/hooks';
 import { useClassStore } from '../stores/class/class.store';
+import { useUserStore } from '../stores/user/user.store';
 
 const CREATE_ADMIN_MUTATION_KEY = '@admin/create';
 
 export const useCreateAdminMutation = () => {
 	const classId: number = useClassStore((state) => state.classId);
+	const myClassesOption = useUserStore((state) => state.myClassesOption);
 	console.log('ADMIN', classId);
-	const { mutateAsync } = useMutation((params: CreateAdminParams) => adminApi.createAdmin(classId, params), {
-		mutationKey: [CREATE_ADMIN_MUTATION_KEY],
-	});
+	const { mutateAsync } = useMutation(
+		(params: CreateAdminParams) => adminApi.createAdmin(classId === -1 ? myClassesOption[0].value : classId, params),
+		{
+			mutationKey: [CREATE_ADMIN_MUTATION_KEY],
+		},
+	);
 
 	const isMutating = useIsMutating([CREATE_ADMIN_MUTATION_KEY]) > 0;
 
