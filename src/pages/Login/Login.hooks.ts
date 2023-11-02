9db22e014c5388772990984302c3695d11b261a1
Hyevvy <hyeg0@naver.com>
2023-11-02 16:12:29 +0900

    💛 [FEAT] refresh 추가

--- src/pages/Login/Login.hooks.ts
+++ src/pages/Login/Login.hooks.ts
@@ -48,24 +48,24 @@ export const useLogin = () => {
 			.catch((err) => {
 				Toast(false, '아이디 비밀번호를 다시 확인해주세요');
 				// useEffect(() => {
-				fetch('https://bitbox.kro.kr/authentication-service/auth/refresh', {
-					method: 'POST',
+				// fetch('https://bitbox.kro.kr/authentication-service/auth/refresh', {
+				// 	method: 'POST',
 
-					headers: {
-						Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-					},
+				// 	headers: {
+				// 		Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+				// 	},
 
-					credentials: 'include',
-				})
-					.then((res) => {
-						console.log(origin);
-						console.log(res.headers.get('set-cookie')); // undefined
-						console.log(document.cookie); // nope
-						return res.json();
-					})
-					.then((res) => {
-						localStorage.setItem('accessToken', res.accessToken);
-					});
+				// 	credentials: 'include',
+				// })
+				// 	.then((res) => {
+				// 		console.log(origin);
+				// 		console.log(res.headers.get('set-cookie')); // undefined
+				// 		console.log(document.cookie); // nope
+				// 		return res.json();
+				// 	})
+				// 	.then((res) => {
+				// 		localStorage.setItem('accessToken', res.accessToken);
+				// 	});
 				// }, []);
 			});
 	};
